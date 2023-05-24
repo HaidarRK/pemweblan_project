dc4ed01b3798894fdfb8b2ec225672b09f51850f
HaidarRK <130622291+HaidarRK@users.noreply.github.com>
2023-05-24 18:37:25 +0700

    Add files via upload

---
+++ storage/framework/views/1803cb580a8b84a0d194618de4a51648269d248e.php
@@ -0,0 +1,71 @@
+
+<?php $__env->startSection('title', 'Kereta'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <a href="/kereta/add" class="btn btn-primary btn-sm">Add</a> <br>
+
+    <?php if(session('Pesan')): ?>
+    <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-check"></i> Success!</h4>
+        <?php echo e(session('Pesan')); ?>.
+    </div>
+    <?php endif; ?>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>No. KA</th>
+                <th>Jadwal Keberangkatan</th>
+                <th>Stasiun Asal</th>
+                <th>Stasiun Tujuan</th>
+                <th>Harga</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $__currentLoopData = $kereta; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <tr>
+                    <td><?php echo e($data->no_ka); ?></td>
+                    <td><?php echo e($data->jadwal_berangkat); ?></td>
+                    <td><?php echo e($data->keberangkatan); ?></td>
+                    <td><?php echo e($data->tujuan); ?></td>
+                    <td><?php echo e($data->harga); ?></td>
+                    <td>
+                        <a href="/kereta/detail/<?php echo e($data->no_ka); ?>" class="btn btn-sm btn-success">Detail</a>
+                        <a href="/kereta/edit/<?php echo e($data->no_ka); ?>" class="btn btn-sm btn-warning">Edit</a>
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?php echo e($data->no_ka); ?>">
+                            Delete
+                        </button>
+                    </td>
+                    
+                </tr>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </tbody>
+    </table>
+
+<?php $__currentLoopData = $kereta; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <div class="modal modal-danger fade" id="delete<?php echo e($data->no_ka); ?>">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title"><?php echo e($data->no_ka); ?></h4>
+                </div>
+                <div class="modal-body">
+                    <p>Apakah Anda Yakin Ingin Menghapus Data Ini?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">No</button>
+                    <a href="/kereta/delete/<?php echo e($data->no_ka); ?>" class="btn btn-outline">Yes</a>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout/v_template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\pbd_uas\resources\views/v_kereta.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f7fc6e20124e77cd1c96e6006468ce882ac0bb9f.php
@@ -0,0 +1,117 @@
+
+<?php $__env->startSection('title', 'Edit Kereta'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<form action="/kereta/update/<?php echo e($kereta->no_ka); ?>" method="POST" enctype="multipart/form-data">
+    <?php echo csrf_field(); ?>
+
+    <div class="content">
+        <div class="row">
+            <div class="col-sm-6">
+
+                <div class="form-group">
+                    <label>No. KA</label>
+                    <input name="no_ka" class="form-control" value="<?php echo e($kereta->no_ka); ?>">
+                    <div class="text-danger">
+                        <?php $__errorArgs = ['no_ka'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <?php echo e($message); ?>
+
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Jadwal Keberangkatan</label>
+                    <input name="jadwal_berangkat" class="form-control" value="<?php echo e($kereta->jadwal_berangkat); ?>">
+                    <div class="text-danger">
+                        <?php $__errorArgs = ['jadwal_berangkat'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <?php echo e($message); ?>
+
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Stasiun Asal</label>
+                    <input name="keberangkatan" class="form-control" value="<?php echo e($kereta->keberangkatan); ?>">
+                    <div class="text-danger">
+                        <?php $__errorArgs = ['keberangkatan'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <?php echo e($message); ?>
+
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Stasiun Tujuan</label>
+                    <input name="tujuan" class="form-control" value="<?php echo e($kereta->tujuan); ?>">
+                    <div class="text-danger">
+                        <?php $__errorArgs = ['tujuan'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <?php echo e($message); ?>
+
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Harga</label>
+                    <input name="harga" class="form-control" value="<?php echo e($kereta->harga); ?>">
+                    <div class="text-danger">
+                        <?php $__errorArgs = ['harga'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <?php echo e($message); ?>
+
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                    </div>
+                </div>
+
+                
+                
+                <div class="col-sm 12">
+                    <div class="form-group">
+                        <button class="btn btn-primary btn-sm">Simpan</button>
+                    </div>
+                </div>
+            
+        </div>
+    </div>
+
+</form>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout/v_template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\pbd_uas\resources\views/v_editkereta.blade.php ENDPATH**/ ?>
