c21cd778804aeb7c62cf8855217f52288d494bbc
HaidarRK <130622291+HaidarRK@users.noreply.github.com>
2023-05-30 19:30:30 +0700

    Delete storage directory

--- storage/framework/views/8686a630a14b2744a3fbea0a4d9d5aef807cd458.php
+++
@@ -1,7 +0,0 @@
-
-<?php $__env->startSection('title', 'Home'); ?>
-
-<?php $__env->startSection('content'); ?>
-    <h1>Selamat datang!</h1>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout/v_template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\pbd_uas\resources\views/v_home.blade.php ENDPATH**/ ?>
